0231ead03bae09b914173b806d32d4acb7fa53b1
patricia.figueirar@gmail.com <patricia.figueirar@gmail.com@66436a29-6145-c042-5ffb-d93278ebf5b3>
2011-12-22 23:57:54 +0000

    Inicio

---
+++ trunk/Desarrollo-Aplicacion/src/desaapplication/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package desaapplication;
+
+import GUI.home;
+
+/**
+ *
+ * @author RS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        home home = new home();
+        home.setVisible(true);
+    }
+
+}
